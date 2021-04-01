dbd0853994de3e55ec524cb06eefd24a27926531
lixiaorong <lixiaorong@novelpro.cn>
2021-04-01 15:29:34 +0800

    xx

--- Controller/BswWebController.php
+++ Controller/BswWebController.php
@@ -37,7 +37,7 @@ abstract class BswWebController extends AbstractController
     /**
      * @var string
      */
-    protected $version = '1.5.56';
+    protected $version = '1.5.57';
 
     /**
      * @var bool

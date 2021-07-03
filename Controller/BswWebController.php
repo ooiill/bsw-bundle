06f255c6a000500cc0e67e568b4cc0e81216f58d
lixiaorong <lixiaorong@novelpro.cn>
2021-07-03 20:10:42 +0800

    结构调整

--- Controller/BswWebController.php
+++ Controller/BswWebController.php
@@ -39,7 +39,7 @@ abstract class BswWebController extends AbstractController
     /**
      * @var string
      */
-    protected $version = '2.1.3';
+    protected $version = '2.1.4';
 
     /**
      * @var string

c395d915c96bb935d40e5348a92035942e19bbe3
lixiaorong <lixiaorong@novelpro.cn>
2022-05-25 11:19:32 +0800

    x

--- Controller/BswWebController.php
+++ Controller/BswWebController.php
@@ -39,7 +39,7 @@ abstract class BswWebController extends AbstractController
     /**
      * @var string
      */
-    protected $version = '2.3.10';
+    protected $version = '2.3.11';
 
     /**
      * @var string

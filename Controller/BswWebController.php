d3de7becdc5319bf1431322c01af957fbcd3bf48
lixiaorong <lixiaorong@shizhuang-inc.com>
2022-06-11 11:12:25 +0800

    2.5.0

--- Controller/BswWebController.php
+++ Controller/BswWebController.php
@@ -39,7 +39,7 @@ abstract class BswWebController extends AbstractController
     /**
      * @var string
      */
-    protected $version = '2.3.11';
+    protected $version = '2.5.0';
 
     /**
      * @var string

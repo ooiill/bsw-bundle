ce21cb825eead076f8ef8bdc8b69ea471abde3fd
lixiaorong <lixiaorong@shizhuang-inc.com>
2022-06-11 11:32:09 +0800

    composer.json

--- Controller/BswWebController.php
+++ Controller/BswWebController.php
@@ -39,7 +39,7 @@ abstract class BswWebController extends AbstractController
     /**
      * @var string
      */
-    protected $version = '2.5.0';
+    protected $version = '2.5.2';
 
     /**
      * @var string
